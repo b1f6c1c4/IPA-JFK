c0fb735879fb480884e3b50fd0b59fd652895459
b1f6c1c4 <b1f6c1c4@gmail.com>
2020-08-27 01:18:46 -0400

    download cmudict

---
+++ scripts/download.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const mkdirp = require('mkdirp');
+const fs = require('fs');
+const https = require('https');
+
+mkdirp.sync('data');
+
+https.get('https://svn.code.sf.net/p/cmusphinx/code/trunk/cmudict/cmudict-0.7b', (resp) => {
+  const f = fs.createWriteStream('data/cmudict');
+  resp.pipe(f);
+}).on('error', (err) => {
+  console.error('Error:', err);
+  process.exit(1);
+});
