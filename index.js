e3e338c85589067362255fd1eb8cec38cc1b3a24
b1f6c1c4 <b1f6c1c4@gmail.com>
2020-08-28 00:32:57 -0400

    unicode

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const htmlencode = require('htmlencode');
+const { htmlEncode } = require('htmlencode');
 const parse = require('./src/parse');
 const syllable = require('./src/syllable');
 const vowel = require('./src/vowel');
@@ -18,8 +18,116 @@ function ir(w) {
   return phs;
 }
 
-function utf8Encode(r) {
-  // TODO
+const vowelUnicode = {
+  ae: { tense: ['e', '\u0259\u032f'], lax: ['\u00e6'] },
+  aI: { tense: ['\u0251\u02d6', '\u026a\u032f'] },
+  aU: { tense: ['a', '\u028a\u032f'] },
+  A: { tense: ['\u0251', '\u02d0'], rhotic: ['\u0252', '\u0259\u032f', '\u02de'] },
+  2: { lax: ['\u028c'] },
+  eI: { tense: ['e\u026a\u02d5\u032f'] },
+  e: { rhotic: ['e\u0259\u02d5\u032f', '\u02de'], lax: ['\u025b'] },
+  '3r': { rhotic: ['\u025c', '\u02de', '\u02d0'] },
+  '@': { rhoticWeak: ['\u0259', '\u02de', '\u02d0'], laxWeak: ['\u0259'] },
+  i: { tense: ['i', '\u02d0'], tenseWeak: ['i', '\u02d0'], rhotic: ['i', '\u0259\u032f', '\u02de'] },
+  I: { lax: ['\u026a\u0308'], laxWeak: ['\u0258'] },
+  oU: { tense: ['o\u02d5', '\u028a\u032f'] },
+  OI: { tense: ['o\u02d5', '\u026a\u032f'] },
+  O: { tense: ['\u0254', '\u0259\u032f'], rhotic: ['o\u02d5\u0259\u032f', '\u02de'], lax: ['\u0254', '\u0259\u032f'] },
+  u: { tense: ['\u028a', 'u\u032f'], tenseWeak: ['\u028a', 'u\u032f'] },
+  U: { rhotic: ['\u028a', '\u0259\u032f', '\u02de'], lax: ['\u028a\u0308'], laxWeak: ['\u0275'] },
+};
+const consonantUnicode = {
+  N: '\u014b',
+  g: '\u0261',
+  T: '\u03b8',
+  D: '\u00f0',
+  S: '\u0283',
+  Z: '\u0292',
+  l: 'l\u02fd',
+  r: '\u0279',
+  c: 'c\u0327',
+};
+
+function utf8Encode(phs) {
+  let state = 'coda';
+  function enc(ph, split) {
+    let rxp = '';
+    let rx = split ? [] : '';
+    let rxs = '';
+    for (let p of ph) {
+      if (Array.isArray(p)) {
+        const [pre, s, sup] = enc(p, true);
+        rx += `${pre}${s.join('\u0361').trim()}${sup}`;
+      } else {
+        let ss;
+        if (p.isVowel) {
+          ss = vowelUnicode[p.pho][p.property + (p.weak ? 'Weak' : '')];
+        } else {
+          ss = [consonantUnicode[p.pho] || p.pho];
+        }
+        if (p.velarized > 0.5)
+          ss = ss.map((s) => `${s}\u0334`);
+        if (p.devoiced)
+          ss = ss.map((s) => `${s}\u02f3`);
+        if (p.nasalized)
+          ss = ss.map((s) => `${s}\u0303`);
+        if (p.retracted)
+          ss = ss.map((s) => `${s}\u02cd`);
+        if (p.raised)
+          ss = ss.map((s) => `${s}\u02d4`);
+        if (p.phono === 'nucleus' && !p.isVowel)
+          ss = ss.map((s) => `${s}\u02cc`);
+        let s = ss.join('');
+        if (p.release === 'silent')
+          s += '\u02fa';
+        if (p.glottalized)
+          s = '\u0294' + s;
+        let sup = '';
+        if (p.release === 'nasal')
+          sup += '\u207f';
+        else if (p.release === 'labial')
+          sup += '\u02e1';
+        if (p.aspirate > 0.7)
+          sup += '\u02b0';
+        if (p.labialized)
+          sup += '\u02b7';
+        let sp = '';
+        switch (state) {
+          case 'onset':
+            state = p.phono;
+            break;
+          case 'coda':
+          case 'nucleus':
+            if (p.phono !== 'coda') {
+              sp = ' ';
+              if (p.stress === 1)
+                sp += '\u02c8';
+              else if (p.stress === 2)
+                sp += '\u02cc';
+            }
+            state = p.phono;
+            break;
+        }
+        if (!split) {
+          rx += sp;
+          rx += s;
+        } else {
+          rxp += sp;
+          rx.push(s);
+        }
+        if (sup) {
+          if (split)
+            rxs += sup;
+          else
+            rx += sup;
+        }
+      }
+    }
+    if (split)
+      return [rxp, rx, rxs.replace('\u02b7\u02b7', '\u02b7')];
+    return rx.trim();
+  }
+  return `[${enc(phs)}]`;
 }
 
 const vowelLaTeX = {
@@ -36,7 +144,7 @@ const vowelLaTeX = {
   I: { lax: ['\\"I'], laxWeak: ['9'] },
   oU: { tense: ['\\|`o', '\\textsubarch{U}'] },
   OI: { tense: ['\\|`o', '\\textsubarch{I}'] },
-  O: { tense: ['O', '\\texsubarch{@}'], rhotic: ['\\|`o\\textsubarch{@}', '\\textrhoticity'], lax: ['O', '\\textsubarch{@}'] },
+  O: { tense: ['O', '\\textsubarch{@}'], rhotic: ['\\|`o\\textsubarch{@}', '\\textrhoticity'], lax: ['O', '\\textsubarch{@}'] },
   u: { tense: ['U', '\\textsubarch{u}'], tenseWeak: ['U', '\\textsubarch{u}'] },
   U: { rhotic: ['U', '\\textsubarch{@}', '\\textrhoticity'], lax: ['\\"U'], laxWeak: ['8'] },
 };
@@ -121,15 +229,15 @@ function latexEncode(phs) {
       }
     }
     if (split)
-      return [rxp, rx.trim(), rxs.replace(/ww/, 'w')];
+      return [rxp, rx.trim(), rxs.replace('ww', 'w')];
     return rx.trim();
   }
-  return `\\textipa{${enc(phs)}}`;
+  return `\\textipa{[${enc(phs)}]}`;
 }
 
 module.exports = {
   unicode: (w) => utf8Encode(ir(w)),
-  html: (w) => htmlencode(utf8Encode(ir(w))),
+  html: (w) => htmlEncode(utf8Encode(ir(w))),
   latex: (w) => latexEncode(ir(w)),
 };
 module.exports.default = module.exports;
