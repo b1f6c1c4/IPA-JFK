b0f08c92ebf5159d7b0ac2e49e21b7fd6c2331b3
b1f6c1c4 <b1f6c1c4@gmail.com>
2020-08-28 05:28:20 -0400

    add license AGPL-3.0-only

--- src/consonant.js
+++ src/consonant.js
@@ -1,3 +1,20 @@
+/* Copyright (C) 2020 b1f6c1c4
+ *
+ * This file is part of IPA-JFK.
+ *
+ * IPA-JFK is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3.
+ *
+ * IPA-JFK is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with IPA-JFK.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 const roundedPhonemes = ['CH', 'JH', 'SH', 'ZH', 'R'];
 const roundPhonemes = ['CH', 'JH', 'SH', 'ZH', 'R', 'W'];
 
